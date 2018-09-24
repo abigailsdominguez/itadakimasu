919962634f1c115ed04ebc4900266cbae35f8406
Abigail Dominguez <abigailsdominguez@gmail.com>
2018-09-24 16:23:52 +0800

    update mobile responsiveness

---
+++ assets/js/home.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$("#backtotop").click(function(){
+		$("html, body").animate({scrollTop: 0 }, "slow");
+		return false;
+    });
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("backtotop").style.display = "block";
+		$("#backtotop").addClass("fadeIn");
+	} else {
+		document.getElementById("backtotop").style.display = "none";
+		$("#backtotop").removeClass("fadeIn");
+	}
+
+}
+
